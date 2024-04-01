0927d2196c17763477dd62fb403cd28f5411f223
mysticmanikk <162731540+mysticmanikk@users.noreply.github.com>
2024-04-01 11:33:36 -0400

    Update Group.java
    Just some change in the division method.

--- Group.java
+++ Group.java
@@ -15,10 +15,16 @@ public class group{
     printedName();
   }
   public int division(int num1, int num2){
-    int x = num1/num2;
-    if(num2 == 0 || num1 % num2 ==0 )
-      throw new ArithmeticException;
-    return x;
+
+    if(num2 == 0 || num1 % num2 !==0 )
+      throw new ArithmeticException("The division is invalid");
+
+    int q=0;
+    while(num1>num2){
+      num1 -= num2;
+      q++;
+    }
+    return q;
   }
   public int multiply(int num1, int num2){
         return num1 * num2;
