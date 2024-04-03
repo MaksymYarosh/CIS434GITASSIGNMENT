ae55bdad0c80454b5b3b108c502f99d68fdbcff7
mysticmanikk <162731540+mysticmanikk@users.noreply.github.com>
2024-04-03 12:30:22 -0400

    Update GroupTest.java

--- GroupTest.java
+++ GroupTest.java
@@ -25,5 +25,37 @@ public class GroupTest{
     @After
     public void tearDown() {
     }
-  
+
+    @Test
+    public void testPrintingName() {
+        GroupTest group = new GroupTest();
+        assertEquals("Manikdeep Kaur LNU", group.printingName());
+    }
+
+    @Test
+    public void testPrintName() {
+        GroupTest group = new GroupTest();
+        assertEquals("Maksym Yarosh", group.printName());
+    }
+
+    @Test
+    public void testPrintedName() {
+        GroupTest group = new GroupTest();
+        assertEquals("Malik Jamal", group.printedName());
+    }
+
+    @Test
+    public void testGetGroupMembers() {
+        // We can, however, test if they run without errors.
+        GroupTest group = new GroupTest();
+        group.getGroupMembers(); // No assertions because it doesn't return anything
+    }
+
+    @Test(expected=ArithmeticException.class)
+    public void testIntegerDivision() {
+        GroupTest group = new GroupTest();
+        assertEquals(2, group.division(8, 4));//Positive division
+        assertEquals(0, group.division(0, 5)); // Testing division by zero
+        assertEquals(-2, group.division(-8,4));//Negative division 
+}
 }
