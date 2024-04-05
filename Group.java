0f37ccf216d23159cbc1f2434ae85e18bcdbac5d
Maksym Yarosh <161786941+MaksymYarosh@users.noreply.github.com>
2024-04-05 14:07:48 -0400

    Update Group.java
    Changed multiply and power methods again to its original

--- Group.java
+++ Group.java
@@ -32,33 +32,9 @@ public class Group{
     }
     return q;
   }
-  public int multiply(int num1, int num2) {
-        if (num1 == 0 || num2 == 0) {
-            return 0;
-        }
-        boolean isNegative = (num1 < 0) ^ (num2 < 0);
-        num1 = Math.abs(num1);
-        num2 = Math.abs(num2);
-
-        int product = 0;
-        for (int i = 0; i < num2; i++) {
-            product += num1;
-        }
-        return isNegative ? -product : product;
-    }
-  public int power(int num1, int num2) {
-        if (num2 < 0) {
-            throw new IllegalArgumentException("Exponent cannot be negative");
-        }
-        if (num2 == 0) {
-            return 1;
-        }
-        int result = 1;
-        for (int i = 0; i < num2; i++) {
-            result = multiply(result, num1);
-        }
-        return result;
-    }
+  public int multiply(int num1, int num2){
+        return num1 * num2;
     
-      
+  public int power(int num1, int num2){
+        return Math.pow(num1, num2);
 }
