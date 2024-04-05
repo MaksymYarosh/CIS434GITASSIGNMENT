f9c6353ceb8b53dfbc69c69ece3b3e9d08775543
Maksym Yarosh <161786941+MaksymYarosh@users.noreply.github.com>
2024-04-05 14:00:12 -0400

    Update Group.java
    Multiply method updated

--- Group.java
+++ Group.java
@@ -32,8 +32,20 @@ public class Group{
     }
     return q;
   }
-  public int multiply(int num1, int num2){
-        return num1 * num2;
+  public int multiply(int num1, int num2) {
+        if (num1 == 0 || num2 == 0) {
+            return 0;
+        }
+
+        boolean isNegative = (num1 < 0) ^ (num2 < 0);
+        num1 = Math.abs(num1);
+        num2 = Math.abs(num2);
+
+        int product = 0;
+        for (int i = 0; i < num2; i++) {
+            product += num1;
+        }
+        return isNegative ? -product : product;
     }
   public int power(int num1, int num2){
         return Math.pow(num1, num2);
